feb6cfb886a32a8a0091b506c9119277cf7adfb2
Abhijeet Singh <142420753+AbhijeetKamboj@users.noreply.github.com>
2025-06-11 23:07:40 +0530

    Add files via upload

---
+++ src/App.jsx
@@ -0,0 +1,37 @@
+import { useState } from 'react'
+import reactLogo from './assets/leetcode.svg'
+import viteLogo from './assets/code-forces.svg'
+import './App.css'
+
+function App() {
+  const [count, setCount] = useState(0)
+  const handleRedirect = () => {
+    window.location.href = '/leetcode-comparator/index.html';
+  };
+  const handleRedirect1 = () => {
+    window.location.href = '/CFInsights-main/index.html';
+  };
+  return (
+    <>
+      <div>
+        <a href="https://codeforces.com" target="_blank">
+          <img src={viteLogo} className="logo" alt="Vite logo" />
+        </a>
+        <a href="https://leetcode.com" target="_blank">
+          <img src={reactLogo} className="logo react" alt="React logo" />
+        </a>
+      </div>
+      <h1>Codeforces + Leetcode</h1>
+      <div className="button-container">
+  <button onClick={handleRedirect1}>Codeforces</button>
+  <button onClick={handleRedirect}>LeetCode</button>
+</div>
+
+      <p className="read-the-docs">
+        Choose the platform where you wanna Analyse yourself
+      </p>
+    </>
+  )
+}
+
+export default App
